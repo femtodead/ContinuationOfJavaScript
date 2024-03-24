c2a414c6df3a4b7e3049dcef4d87d2e4a7144da4
Александр <femt0dead3@mail.ru>
2024-03-24 10:51:08 +0300

    task_5

---
+++ task_5/subtask_2/data.js
@@ -0,0 +1,44 @@
+const bd = `[
+    {
+        "p": "параграф1",
+        "h1": "заголовок1",
+        "imgSrc": "https://img.freepik.com/free-photo/breathtaking-shot-of-beautiful-stones-under-turquoise-water-of-a-lake-and-hills-in-the-background_181624-12847.jpg?w=1380&t=st=1711258926~exp=1711259526~hmac=2b55ec9abf57fadaee0ffcfa2badde93e8b50e2e6a5391baab3e29a46af69bda",
+        "imgAlt": "картинка1",
+        "style": [
+        "color: black",
+        "width: 20%",
+        "display: flex",
+        "justify-content: center",
+        "height: 400px",
+        "align-items: center"]
+    },
+    {
+        "p": "параграф2",
+        "h1": "заголовок2",
+        "imgSrc": "https://img.freepik.com/free-photo/a-painting-of-a-mountain-lake-with-a-mountain-in-the-background_188544-9126.jpg?w=1380&t=st=1711258849~exp=1711259449~hmac=6338f2a4095175f5630c24e1d32ed791af6d308913d2dad1fdc60ebf81723f84",
+        "imgAlt": "картинка2",
+        "style": [
+            "color: red",
+            "width: 20%",
+            "display: flex",
+            "justify-content: center",
+            "height: 400px",
+            "align-items: center"]
+    },
+    {
+        "p": "параграф3",
+        "h1": "заголовок3",
+        "imgSrc": "https://img.freepik.com/free-photo/forest-landscape_71767-127.jpg?w=996&t=st=1711258951~exp=1711259551~hmac=1da0f3d6441fabf0d555e5023a2096d5eeeb83245db1e7a8a62f56a76c2b106b",
+        "imgAlt": "картинка3",
+        "style": [
+        "color: green",
+        "width: 20%",
+        "display: flex",
+        "justify-content: center",
+        "height: 400px",
+        "align-items: center"]
+    }
+]
+`;
+
+
