32fb9cf77790c58c7da19fdf0660ec13fb5f141e
Александр <femt0dead3@mail.ru>
2024-03-26 22:00:19 +0300

    task_6

---
+++ task_6/data.js
@@ -0,0 +1,115 @@
+const bd = `
+[{
+    "imgCard" : "./img/Rectangle15copy.svg",
+    "imgButton" : "./img/Forma1copy.svg",
+    "buttonText" : "Add to Cart",
+    "cardName" : "ELLERY X M'O CAPSULE",
+    "carDes" : "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "cardPrice" : "$52.00"
+},
+{
+    "imgCard" : "./img/Rectangle15copy(1).svg",
+    "imgButton" : "./img/Forma1copy.svg",
+    "buttonText" : "Add to Cart",
+    "cardName" : "ELLERY X M'O CAPSULE",
+    "carDes" : "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "cardPrice" : "$52.00"
+},
+{
+    "imgCard" : "./img/Rectangle15copy(2).svg",
+    "imgButton" : "./img/Forma1copy.svg",
+    "buttonText" : "Add to Cart",
+    "cardName" : "ELLERY X M'O CAPSULE",
+    "carDes" : "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "cardPrice" : "$52.00"
+},
+{
+    "imgCard" : "./img/Rectangle15copy(3).svg",
+    "imgButton" : "./img/Forma1copy.svg",
+    "buttonText" : "Add to Cart",
+    "cardName" : "ELLERY X M'O CAPSULE",
+    "carDes" : "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "cardPrice" : "$52.00"
+},
+{
+    "imgCard" : "./img/Rectangle15copy(4).svg",
+    "imgButton" : "./img/Forma1copy.svg",
+    "buttonText" : "Add to Cart",
+    "cardName" : "ELLERY X M'O CAPSULE",
+    "carDes" : "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "cardPrice" : "$52.00"
+},
+{
+    "imgCard" : "./img/Rectangle15copy(5).svg",
+    "imgButton" : "./img/Forma1copy.svg",
+    "buttonText" : "Add to Cart",
+    "cardName" : "ELLERY X M'O CAPSULE",
+    "carDes" : "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "cardPrice" : "$52.00"
+},
+{
+    "imgCard" : "./img/Rectangle15copy.svg",
+    "imgButton" : "./img/Forma1copy.svg",
+    "buttonText" : "Add to Cart",
+    "cardName" : "ELLERY X M'O CAPSULE",
+    "carDes" : "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "cardPrice" : "$1.00"
+},
+{
+    "imgCard" : "./img/Rectangle15copy(1).svg",
+    "imgButton" : "./img/Forma1copy.svg",
+    "buttonText" : "Add to Cart",
+    "cardName" : "ELLERY X M'O CAPSULE",
+    "carDes" : "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "cardPrice" : "$2.00"
+},
+{
+    "imgCard" : "./img/Rectangle15copy(2).svg",
+    "imgButton" : "./img/Forma1copy.svg",
+    "buttonText" : "Add to Cart",
+    "cardName" : "ELLERY X M'O CAPSULE",
+    "carDes" : "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "cardPrice" : "$3.00"
+},
+{
+    "imgCard" : "./img/Rectangle15copy(3).svg",
+    "imgButton" : "./img/Forma1copy.svg",
+    "buttonText" : "Add to Cart",
+    "cardName" : "ELLERY X M'O CAPSULE",
+    "carDes" : "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "cardPrice" : "$4.00"
+},
+{
+    "imgCard" : "./img/Rectangle15copy(4).svg",
+    "imgButton" : "./img/Forma1copy.svg",
+    "buttonText" : "Add to Cart",
+    "cardName" : "ELLERY X M'O CAPSULE",
+    "carDes" : "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "cardPrice" : "$5.00"
+},
+{
+    "imgCard" : "./img/Rectangle15copy(5).svg",
+    "imgButton" : "./img/Forma1copy.svg",
+    "buttonText" : "Add to Cart",
+    "cardName" : "ELLERY X M'O CAPSULE",
+    "carDes" : "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "cardPrice" : "$6.00"
+},
+{
+    "imgCard" : "./img/Rectangle15copy.svg",
+    "imgButton" : "./img/Forma1copy.svg",
+    "buttonText" : "Add to Cart",
+    "cardName" : "ELLERY X M'O CAPSULE",
+    "carDes" : "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "cardPrice" : "$7.00"
+},
+{
+    "imgCard" : "./img/Rectangle15copy(1).svg",
+    "imgButton" : "./img/Forma1copy.svg",
+    "buttonText" : "Add to Cart",
+    "cardName" : "ELLERY X M'O CAPSULE",
+    "carDes" : "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "cardPrice" : "$8.00"
+}]
+
+`
